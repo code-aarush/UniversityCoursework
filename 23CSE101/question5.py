1a27246e15a0ac046ed9eef9b6b3b24f31aac045
aarush-coder <aarush.sivaraman@gmail.com>
2025-09-23 22:34:09 +0530

    Question 5 converted to menu-drive

--- 23CSE101/question5.py
+++ 23CSE101/question5.py
@@ -1,8 +1,8 @@
-operation = str(input("Do you want to find [square root] or [cube root]:")) # fetching the operation from user
+operation = str(input("ENTER 1 TO FIND SQUARE ROOT \nENTER 2 TO FIND CUBE ROOT : ")) # fetching the operation from user
 num = float(input("Enter number to find square/cube root of: ")) # getting number
-if operation == "square root" :
+if operation == "1" :
     sqrt = num ** (1/2)
     print("Square root is", sqrt)
-elif operation == "cube root" : 
+elif operation == "2" : 
     cbrt = num ** (1/3)
-    print("Cube root is", cbrt)
+    print("Cube root is", cbrt)             
