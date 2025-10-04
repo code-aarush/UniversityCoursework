2d65f95f8dc914127dd6a53eb104310207740836
code-aarush <aarush.sivaraman@gmail.com>
2025-10-04 12:12:52 +0530

    Delete 23CSE101/question4.py

--- 23CSE101/question4.py
+++
@@ -1,3 +0,0 @@
-temp_F = int(input("Enter temperature in Fahrenheit: ")) # input in fahrenheit
-temp_C = 5/9 * (temp_F - 32) # conversion formula
-print("Temperature in celcius is", temp_C , "C") # output
