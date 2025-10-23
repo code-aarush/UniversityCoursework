b8eceeb11f700c46c46c834a9677c78da30cbbb7
aarush-coder <aarush.sivaraman@gmail.com>
2025-10-23 11:51:53 +0530

    unknown

---
+++ 23CSE101/RUNLENGTHENCODING.py
@@ -0,0 +1,16 @@
+def runLengthEncoding(word):
+    current_counter = 1
+    new_str = ""
+    for i in range(1, len(word)):
+        if word[i] == word[i - 1]:
+            current_counter += 1
+        elif word[i] != word[i - 1]:
+            new_str = str(new_str) + str(word[i - 1]) + str(current_counter)
+            current_counter = 1
+            current_char = word[i]
+
+    new_str = str(new_str) + str(current_char) + str(current_counter)
+    print(new_str)
+
+word = str(input("Enter a word to compress: "))
+print(runLengthEncoding(word))
