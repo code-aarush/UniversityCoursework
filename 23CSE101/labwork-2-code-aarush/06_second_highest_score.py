6252f538b048b0c2954cce5c944eb32f43aee5ca
aarush-coder <aarush.sivaraman@gmail.com>
2025-11-07 21:03:48 +0530

    Question 6 done

--- 23CSE101/labwork-2-code-aarush/06_second_highest_score.py
+++ 23CSE101/labwork-2-code-aarush/06_second_highest_score.py
@@ -22,10 +22,28 @@ scores = list(map(int, input("Enter scores: ").split()))
 
 # TODO 1 – Manual: track largest and second largest.
 second_manual = 0
+
+largest = float('-inf')
+second_largest = float('-inf')
+
+    
+for score in scores:
+        if score > largest:
+            second_largest = largest
+            largest = score
+
+        elif score > second_largest:
+            second_largest = score
+            
+second_manual = second_largest
+
 # HINT: iterate and update 'largest' and 'second'
 
 # TODO 2 – Built-in: sort descending and pick the second distinct value.
 second_builtin = 0
+
+sorted_scores = sorted(scores, reverse=True)
+second_builtin = sorted_scores[1]
 # HINT: sorted(scores, reverse=True)
 
 print(second_manual)
