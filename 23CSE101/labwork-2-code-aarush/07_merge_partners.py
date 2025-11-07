c3c8c9e7cacb28b15f3b21811e12695ca33cbb9c
aarush-coder <aarush.sivaraman@gmail.com>
2025-11-07 20:34:18 +0530

    Question 7 done

--- 23CSE101/labwork-2-code-aarush/07_merge_partners.py
+++ 23CSE101/labwork-2-code-aarush/07_merge_partners.py
@@ -25,10 +25,33 @@ b = input("List B: ").split()
 
 # TODO 1 – Manual merging with index counters.
 merged_manual = []
+i = 0
+j = 0
+
+while i < len(a) or j < len(b):
+    if i < len(a):
+        merged_manual.append(a[i])
+        i += 1
+    if j < len(b):
+        merged_manual.append(b[j])
+        j += 1
+
 # HINT: use i, j indices and alternate appends
 
 # TODO 2 – Use zip() then append leftover elements.
 merged_builtin = []
+merged_builtin = []
+
+for x, y in zip(a, b):
+    merged_builtin.append(x)
+    merged_builtin.append(y)
+
+if len(a) > len(b):
+    merged_builtin.extend(a[len(b):])
+else:
+    merged_builtin.extend(b[len(a):])
+
+
 # HINT: for x,y in zip(a,b): out += [x,y]; then extend the remainder
 
 print(" ".join(merged_manual))
